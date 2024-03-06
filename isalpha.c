d3d06f94e37f160443ae42aa70835757e207da2b
Josef Elias <jelias@c3r4s2.42prague.com>
2024-03-06 17:37:55 +0100

    "the first push, isalpha"

---
+++ isalpha.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   isalpha.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jelias <jelias@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 16:26:51 by jelias            #+#    #+#             */
+/*   Updated: 2024/03/06 16:56:41 by jelias           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_isalpha(int c);
+
+int	ft_isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
+/*
+int	main(void)
+{
+	char	c;
+
+	c = '5';
+
+	if(ft_isalpha(c))
+		printf("it's quite a letter!");
+	else
+		printf("not a letter");
+}
+*/
