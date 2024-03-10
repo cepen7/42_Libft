17ae14110b873ad41844e9c31207cf734172f84a
Josef Elias <josefelias7@gmail.com>
2024-03-10 14:31:37 +0100

    ft_isalnum added

---
+++ README.md
@@ -0,0 +1,1 @@
+A hand-made version of the C standard library, called Libft. This is a project for 42 Prague, a coding school.
--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jelias <jelias@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/10 13:19:33 by jelias            #+#    #+#             */
-/*   Updated: 2024/03/10 14:21:37 by jelias           ###   ########.fr       */
+/*   Created: 2024/03/10 14:06:43 by jelias            #+#    #+#             */
+/*   Updated: 2024/03/10 14:20:16 by jelias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_isdigit(int c);
+int	ft_isalnum(int c);
 
-int	ft_isdigit(int c)
+int	ft_isalnum(int c)
 {
-	if ((c >= 48) && (c <= 57))
+	if (((c > 64 && c < 91) || (c > 96 && c < 123)) || ((c > 47) && (c < 58)))
 		return (1);
 	else
 		return (0);
@@ -26,11 +26,10 @@ int	main(void)
 {
 	char	c;
 
-	c = 'a';
-	if (ft_isdigit(c))
-		printf("it's quite a number!");
+	c = '*';
+	if (ft_isalnum(c))
+		printf("it's alnum :)!");
 	else
-		printf("not a number :(");
-	return (0);
+		printf("not an alnum character :(");
 }
 */
