670e098a943353241015411a46385abdab2824a1
Josef Elias <josefelias7@gmail.com>
2024-03-15 11:49:45 +0100

    ft_memset in progress

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: jelias <jelias@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 23:41:23 by jelias            #+#    #+#             */
-/*   Updated: 2024/03/12 23:50:44 by jelias           ###   ########.fr       */
+/*   Updated: 2024/03/15 11:48:28 by jelias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,25 @@
 void	*ft_memset(void *s, int c, size_t n);
 
 void	*ft_memset(void *s, int c, size_t n)
-// first try, my dude
 {
-	int	i;
+	size_t				i;
+	unsigned	char	*str = s;
 
 	i = 0;
 	while (n >i)
 	{
-		*s = c;
-		s++;
+		str[i] = c;
+		i++;
 	}
 	return (0);
 }
+/*
+int	main(void)
+{
+	char test_text[20] = "I love my doggo";
+
+	ft_memset(test_text, '.', 6);
+	printf("%s", test_text);
+	return (0);
+}
+*/
