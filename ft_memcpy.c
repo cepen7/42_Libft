33ee995a98e4b44bed9d22cf288059cd19e28bc8
Josef Elias <josefelias7@gmail.com>
2024-04-14 23:50:20 +0200

    ft_memcpy now norminette OK

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,45 +6,43 @@
 /*   By: jelias <jelias@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 10:31:52 by jelias            #+#    #+#             */
-/*   Updated: 2024/04/14 00:13:21 by jelias           ###   ########.fr       */
+/*   Updated: 2024/04/14 23:49:26 by jelias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void    *ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
-void    *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-    char    *d;
-    const char    *s;
-    void    *orig_dest;
-    int     i;
+	char		*d;
+	const char	*s;
+	void		*orig_dest;
+	int			i;
 
-    d = dest;
-    s = src;
-    orig_dest = dest;
-
-
-    if (!dest || !src)
-        return (dest);
-
-    i = 0;
-    while (n > 0)
-    {
-        d[i] = s[i];
-        i++;
-        n--;
-    }
-    return orig_dest;
+	d = dest;
+	s = src;
+	orig_dest = dest;
+	if (!dest || !src)
+		return (dest);
+	i = 0;
+	while (n > 0)
+	{
+		d[i] = s[i];
+		i++;
+		n--;
+	}
+	return (orig_dest);
 }
+/*
+int	main(void)
+{
+	char	source[] = "How do you do, fellow kids?";
+	char	destination[50];
 
-int    main(void){
-    char    source[] = "How do you do, fellow kids?";
-    char    destination[50];
-
-    ft_memcpy(destination, source, 13);
-    printf("%s", destination);
-    return (0);
+	ft_memcpy(destination, source, 30);
+	printf("%s", destination);
+	return (0);
 }
-
+*/
--- libft.h
+++ libft.h
@@ -13,5 +13,6 @@ int	ft_isprint(int c);
 size_t	strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+void    *ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
