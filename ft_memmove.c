388ee1ccc8636afee37381615aa57dd1252b4273
Josef Elias <josefelias7@gmail.com>
2024-04-15 23:56:32 +0200

    ft_memmove.c added

---
+++ ft_memmove.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jelias <jelias@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 23:27:41 by jelias            #+#    #+#             */
+/*   Updated: 2024/04/15 23:55:56 by jelias           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	*memmove(void *dest, cost void *src, size_t n);
+
+void	*memmove(void *dest, cost void *src, size_t n)
+{
+/*there should be a buffer to first copy n bytes from source
+to then copy them to dest wihout modyfying the values in case
+the addresses overlap*/
+
+
+}
